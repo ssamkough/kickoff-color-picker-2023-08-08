fe4764ab1d0f087dc363fb2f856577a8bee46ce9
Sammy <sammysamkough@hotmail.com>
2024-08-27 21:56:58 -0400

    better empty state

--- components/palettes/Palette.tsx
+++ components/palettes/Palette.tsx
@@ -17,18 +17,23 @@ const Palette = ({ palette }: Props) => {
         flex: 1,
       }}
     >
-      {formattedPalette.length > 0
-        ? formattedPalette.map(({ red, green, blue }) => (
-            <div
-              key={`${red}-${green}-${blue}`}
-              style={{
-                width: 40,
-                height: 40,
-                backgroundColor: `rgb(${red}, ${green}, ${blue})`,
-              }}
-            ></div>
-          ))
-        : "No colors added to palette yet!"}
+      {formattedPalette.length > 0 ? (
+        formattedPalette.map(({ red, green, blue }) => (
+          <div
+            key={`${red}-${green}-${blue}`}
+            style={{
+              width: 40,
+              height: 40,
+              backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+            }}
+          ></div>
+        ))
+      ) : (
+        <div style={{ width: 250 }}>
+          No colors added to palette yet! You must have 5 colors to upload a
+          palette!
+        </div>
+      )}
     </div>
   );
 };
